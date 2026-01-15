e83f37c9817dfd4e0fbe070bd311ca3c504fb1ca
Alder <alder0216@gmail.com>
2026-01-15 11:10:44 -0800

    changes basic sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,38 @@
+let bg;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth,windowHeight);
+  bg = color(255,0,0)
 }
 
 function draw() {
-  background(255, 0, 0);
-  ellipse(mouseX, mouseY, 50, 50);
+  background(bg);
+  // ellipse(mouseX, mouseY, 50, 50);
+  // line(0, height/2, width, 0)
+// for(let i = 0; i < 4; i++) {
+//   rect(i * 100, 0, 100, 100);
+// }
+// translate(width/2, height/2);
+// beginShape();
+// for(let i = 0; i < 10; i++) {
+//   const x = random(-100, 100);
+//   const y = random(-100, 100);
+//   vertex(x, y);
+// }
+// endShape();
+// }
+bezier(0, 0, 100, 200, 150, 250, width, height);
+myTriangle(.05, mouseX, mouseY);
+console.log("mouseX: "+mouseX, "mouseY: "+mouseY);
+}
+function windowResized() {
+  resizeCanvas(windowWidth,windowHeight);
+  bg = color(random(255), random(150),random(255));
+}
+function myTriangle(amountOfWindow, x, y){
+  push();
+  const size = width * amountOfWindow;
+  translate(x,y);
+  triangle(0,0, size, size*2, -size, size*2);
+  pop();
 }
