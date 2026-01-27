0b3b4ce525c97fdf1d83fe9282a14ffc33ff26c1
Alder <alder0216@gmail.com>
2026-01-27 11:41:39 -0800

    finalized sketch, second curve leans now

--- sketch.js
+++ sketch.js
@@ -12,21 +12,17 @@ function setup() {
 
 function draw() {
   background(bg);
+
 console.log(mouseX,mouseY)
 bezier(0, 0,305, y, x, 50, width, height);
 // x += random(-speed, speed); //redraw at random vertical direction between -2.5 and 2.5
 // y += random(-speed, speed);
-fill(150,y+150,x+150)
-bezier(width/2,0, 800,400 ,width/5,height/5, width/2, height/2);
+fill(y,100,x)
+bezier(width/2,0, 800,400 ,width/5,height/5, x, y);
+
 }
 function windowResized() {
   resizeCanvas(windowWidth,windowHeight);
   bg = color(x, random(150),random(255));
+
 }
-// function myTriangle(amountOfWindow, x, y){
-//   push();
-//   const size = width * amountOfWindow;
-//   translate(x,y);
-//   triangle(0,0, size, size*2, -size, size*2);
-//   pop();
-// }
